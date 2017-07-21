7aea73e6da75cfce5e193d79c882e931d048f3fa
黄振华 <471426906@qq.com>
2017-07-21 10:16:48 +0800

    初始各个控制器

---
+++ Weight-loss-plans/Weight-loss-plans/Classes/View/Sport/HZHSportController.swift
@@ -0,0 +1,26 @@
+//
+//  HZHSportController.swift
+//  Weight-loss-plans
+//
+//  Created by 黄振华 on 2017/7/21.
+//  Copyright © 2017年 黄振华. All rights reserved.
+//
+
+import UIKit
+
+class HZHSportController: HZHBaseController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
