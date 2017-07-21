82c5dd6503e54d0519cd391fabdd6dda0d887b0e
黄振华 <471426906@qq.com>
2017-07-21 09:55:23 +0800

    项目初始化

---
+++ Weight-loss-plans/Weight-loss-plans/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weight-loss-plans
+//
+//  Created by 黄振华 on 2017/7/21.
+//  Copyright © 2017年 黄振华. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
